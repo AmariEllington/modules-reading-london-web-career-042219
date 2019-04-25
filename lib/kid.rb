f2d8e8864e064f218d5917a1f00ba0f006a75e41
Amari Ellington <amari.l.ellington@gmail.com>
2019-04-25 15:30:52 +0000

    Automatically backed up by Learn

--- lib/kid.rb
+++ lib/kid.rb
@@ -4,13 +4,11 @@ require_relative './class_methods_module.rb'
 
 class Kid
   include Dance
-
-    attr_accessor :name
-
-    def initialize(name)
-      @name = name
-    end
+ 
+  attr_accessor :name
+ 
+  def initialize(name)
+    @name = name
   end
-
-
+end
 end
